393a1077b599ef178298052dd7e86cf8cd9b849a
ranjun <openote@126.com>
2024-12-01 16:32:55 +0800

    control status bar show

---
+++ design-code/design-code/Styles/Animations.swift
@@ -0,0 +1,8 @@
+//
+//  Animations.swift
+//  design-code
+//
+//  Created by 7yue on 2024/12/1.
+//
+
+import Foundation
---
+++ design-code/design-code/Views/Exp/StateBar.swift
@@ -0,0 +1,18 @@
+//
+//  StateBar.swift
+//  design-code
+//
+//  Created by 7yue on 2024/12/1.
+//
+
+import SwiftUI
+
+struct StateBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateBar()
+}
