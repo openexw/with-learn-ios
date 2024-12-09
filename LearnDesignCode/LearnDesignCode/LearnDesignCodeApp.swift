ed5ed95c469035c33d3d465cad98e8236403cc86
ranjun <openote@126.com>
2024-12-09 20:16:33 +0800

    init projec

---
+++ LearnDesignCode/LearnDesignCode/LearnDesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnDesignCodeApp.swift
+//  LearnDesignCode
+//
+//  Created by 7yue on 2024/12/9.
+//
+
+import SwiftUI
+
+@main
+struct LearnDesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
